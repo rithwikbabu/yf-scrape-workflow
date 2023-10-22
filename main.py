9b9c4e2a6ea50eb2124d6d0c9b6d48faecc35339
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-10-22 15:06:41 -0500

    updated workflow

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 import re
 import json
+from datetime import datetime
 
 # Initialize ChromeDriver
 service = Service(executable_path='/usr/bin/chromedriver')
@@ -42,4 +43,20 @@ for box in news_boxes:
             news_links.append(link_dict)
             break
         
-print(news_links)
+# Remove duplicates
+unique_links = {}
+for d in news_links:
+    link = d["link"]
+    if link not in unique_links:
+        unique_links[link] = d
+    else:
+        existing_entry = unique_links[link]
+        if existing_entry["publisher"] is None and d["publisher"] is not None:
+            unique_links[link] = d
+            
+news_links = list(unique_links.values())
+
+with open(datetime.now() + "_date.json", "w") as json_file:
+    json.dump(news_links, json_file, default=str)
+
+driver.quit()

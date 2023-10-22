3ad3a1d3415b0ac4629eedd35e0498697e6da3c0
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-10-22 13:51:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-if __name__ == "__main__":
-  print("WORKFLOW SUCESS")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Initialize ChromeDriver
+driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+
+# Open Google
+driver.get("https://www.google.com")
+
+# Find the search box using its name attribute value
+search_box = driver.find_element_by_name("q")
+
+# Type in the search query
+search_box.send_keys("Hello, world!")
+
+# Submit the query (like hitting 'Enter' key)
+search_box.send_keys(Keys.RETURN)
+
+# Wait for search results to load
+time.sleep(2)
+
+# Print the title of the current page
+print("Title of the page is: ", driver.title)
+
+# Close the browser
+driver.quit()

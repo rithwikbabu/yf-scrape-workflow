ae90bbf3b92872457df9a457676ec677165b8282
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-10-24 14:13:42 -0500

    updated workflow

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import time
 import re
-import json
-from datetime import datetime
 
 # Initialize ChromeDriver
 service = Service(executable_path='/usr/bin/chromedriver')
@@ -14,49 +11,18 @@ driver = webdriver.Chrome(service=service, options=options)
 
 driver.get(f'https://finance.yahoo.com/news/')
 
-news_boxes = driver.find_elements(By.CLASS_NAME, 'Cf')
-patterns = {
-    "news": re.compile(r'https://finance\.yahoo\.com/news/.+\.html'),
-    "media": re.compile(r'https://finance\.yahoo\.com/m/.+\.html'),
-    "video": re.compile(r'https://finance\.yahoo\.com/video/.+\.html'),
-}
+news_links = driver.find_elements(By.XPATH, '//a[contains(@href, "https://finance.yahoo.com/news/")]')
 
-def extract_link_data(box):
-    link_dict = {"link": None, "publisher": None, "type": None}
-    s = box.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
-    box_text = re.split('\n', box.text)
+# Extract and store the links in a list
+links_list = []
+for link in news_links:
+    href = link.get_attribute('href')
+    if href:
+        links_list.append(href)
 
-    for string in box_text:
-        if "•" in string:
-            link_data = string.split("•")
-            link_dict["publisher"] = link_data[0]
-    return s, link_dict
-
-news_links = []
-
-for box in news_boxes:
-    link, link_dict = extract_link_data(box)
-    for category, pattern in patterns.items():
-        if pattern.match(link):
-            link_dict["link"] = link
-            link_dict["type"] = category
-            news_links.append(link_dict)
-            break
-        
-# Remove duplicates
-unique_links = {}
-for d in news_links:
-    link = d["link"]
-    if link not in unique_links:
-        unique_links[link] = d
-    else:
-        existing_entry = unique_links[link]
-        if existing_entry["publisher"] is None and d["publisher"] is not None:
-            unique_links[link] = d
-            
-news_links = list(unique_links.values())
-
-with open(str(datetime.now()) + "_date.json", "w") as json_file:
-    json.dump(news_links, json_file, default=str)
+# Write the links to a file
+with open('links.txt', 'w') as file:
+    for link in links_list:
+        file.write(link + '\n')
 
 driver.quit()

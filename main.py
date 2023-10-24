685d5fca6522d8d0a6947403a4c986b04d3b07d8
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-10-24 14:49:54 -0500

    minor bug

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 import re
 
 # Initialize ChromeDriver
@@ -11,18 +12,20 @@ driver = webdriver.Chrome(service=service, options=options)
 
 driver.get(f'https://finance.yahoo.com/news/')
 
-news_links = driver.find_elements(By.XPATH, '//a[contains(@href, "https://finance.yahoo.com/news/")]')
+news_boxes = driver.find_elements(By.CLASS_NAME, 'Cf')
+pattern = re.compile(r'https://finance\.yahoo\.com/news/.+\.html')
 
-# Extract and store the links in a list
-links_list = []
-for link in news_links:
-    href = link.get_attribute('href')
-    if href:
-        links_list.append(href)
+# To hold unique links
+unique_links = set()
 
-# Write the links to a file
-with open('links.txt', 'w') as file:
-    for link in links_list:
-        file.write(link + '\n')
+for box in news_boxes:
+    link = box.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+    if pattern.match(link):
+        unique_links.add(link)
+
+# Save unique links to a file
+with open('links.txt', 'w') as f:
+    for link in unique_links:
+        f.write(f"{link}\n")
 
 driver.quit()
